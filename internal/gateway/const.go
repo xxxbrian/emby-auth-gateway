a43096ec3acde0ca1b75681c75dc8c7adb455243
Bojin Li <git@bojin.li>
2026-07-09 01:19:42 +1000

    chore: bump version to 0.3.4

--- internal/gateway/const.go
+++ internal/gateway/const.go
@@ -4,7 +4,7 @@ import "time"
 
 const (
 	defaultSessionTTL = 30 * 24 * time.Hour
-	gatewayVersion    = "0.3.3"
+	gatewayVersion    = "0.3.4"
 
 	backendAuthTimeout              = 15 * time.Second
 	proxyResponseHeaderTimeout      = 30 * time.Second
